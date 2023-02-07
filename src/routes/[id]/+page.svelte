a09ec01ec924424769340f68291bc7a37f9f009b
KolyStudio <97678364+KolyStudio@users.noreply.github.com>
2023-02-07 18:24:24 +0700

    first commit

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -58,7 +58,7 @@
 	}
 
 	  
-	  let link = `https://k.schnell-treffen.com/?abc=2ab0b5837e6c2796&xa=n&acme=wid.90880&media=social&tpls=9&v=sexy&userPhotos=45&userDistance=8&userName=MissCandy&userPicture=https://i.ibb.co/c6Tn6mZ/final.webp&s1=snap&s2=` + date
+	  let link = `https://k.schnell-treffen.com/?abc=2ab0b5837e6c2796&xa=n&acme=wid.90880&media=social&tpls=9&v=sexy&userPhotos=45&userDistance=8&userName=MissCandy&userPicture=https://i.ibb.co/7Wg8bsG/1675742592957.jpg&s1=snap&s2=` + date
 
 	function smartLink() {
 		setTimeout(() => {

4532397b34f66e22e4e16e6732684e7b2040d222
KolyStudio <97678364+KolyStudio@users.noreply.github.com>
2023-02-21 19:18:01 +0700

    first commit

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -61,7 +61,7 @@
 	}
 
 	  
-	  let link = `https://k.schnell-treffen.com/?abc=2ab0b5837e6c2796&xa=n&acme=wid.90880&media=social&tpls=9&v=sexy&userPhotos=45&userDistance=8&userName=MissCandy&userPicture=https://mapagesecrete.com/final.webp&s1=cash&s2=` + date
+	//   let link = `https://k.schnell-treffen.com/?abc=2ab0b5837e6c2796&xa=n&acme=wid.90880&media=social&tpls=9&v=sexy&userPhotos=45&userDistance=8&userName=MissCandy&userPicture=https://mapagesecrete.com/final.webp&s1=cash&s2=` + date
 
 
 	  

6b0f490130e16e10aa9c0f06442b6cb5e407c3dc
KolyStudio <97678364+KolyStudio@users.noreply.github.com>
2023-02-24 16:25:54 +0700

    first commit

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -3,18 +3,12 @@ export { matchers } from './client-matchers.js';
 			export const nodes = [() => import('./nodes/0'),
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
-	() => import('./nodes/3'),
-	() => import('./nodes/4'),
-	() => import('./nodes/5'),
-	() => import('./nodes/6')];
+	() => import('./nodes/3')];
 
 			export const server_loads = [];
 
 			export const dictionary = {
 	"": [2],
-	"plp/[id]": [4],
-	"pof/[id]": [5],
-	"s/[id]": [6],
 	"[id]": [3]
 };
 
